a143acc9860789181ccf1aaad4c61aaf6ce94b80
hargun3045 <hargun3045@gmail.com>
2021-08-07 23:22:21 +0530

    removed some code

--- script.py
+++ script.py
@@ -1,15 +1,4 @@
-import sys
-
 import requests
-
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = f"Hello, {who_to_greet}"
-    return greeting
-
 
 r = requests.get("https://hargun3045.github.io")
 print(r.status_code)
